d58a550d300a99834fd05a953bca3c9a91220747
Josh Turner <cs.pmosh@gmail.com>
2020-04-02 10:37:04 -0500

    Updates

---
+++ precache-manifest.9b067fdfa8a4f11eea772df8264ea1b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c558481396587e91f4a6d5abf082878",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3b41ca5267925d019ea8",
+    "url": "/portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "1287754b12e0ca49affb",
+    "url": "/portfolio/static/js/2.59ba3042.chunk.js"
+  },
+  {
+    "revision": "bc51ad6b075e5b96cf9ad2ad3d22ac0d",
+    "url": "/portfolio/static/js/2.59ba3042.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b41ca5267925d019ea8",
+    "url": "/portfolio/static/js/main.15122c5e.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  }
+]);
